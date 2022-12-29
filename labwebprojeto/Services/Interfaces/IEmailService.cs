98eb049ddaa753e4542b1cf3f62eb820fdf95249
Surprise <al74044@utad.eu>
2022-12-29 16:20:29 +0000

    Add Services and ViewModels

---
+++ labwebprojeto/Services/Interfaces/IEmailService.cs
@@ -0,0 +1,29 @@
+﻿using SendGrid.Helpers.Mail;
+using SendGrid;
+
+namespace labwebprojeto.Services.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string toEmail, string subject, string content);
+    }
+
+    public class SendGridMailService : IEmailService
+    {
+        private IConfiguration _configuration;
+
+        public SendGridMailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task SendEmailAsync(string toEmail, string subject, string content)
+        {
+            var apiKey = _configuration["SendGridAPIKey"];
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("franciscoggouveia@gmail.com", "Can't Stop WebSite - Shop Online");
+            var to = new EmailAddress(toEmail);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, content, content);
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
---
+++ labwebprojeto/ViewModels/CreateJogoViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace labwebprojeto.ViewModels
+{
+    public class CreateJogoViewModel
+    {
+        public int IdJogos { get; set; }
+        public string Nome { get; set; } = null!;
+        public IFormFile Foto { get; set; } = null!;
+        public IFormFile Foto1 { get; set; } = null!;
+        public IFormFile Foto2 { get; set; } = null!;
+        public int IdCategoria { get; set; }
+        public int IdConsola { get; set; }
+        public int IdProdutora { get; set; }
+        public decimal Preco { get; set; }
+        public string Descricao { get; set; } = null!;
+        public string Descricao1 { get; set; } = null!;
+    }
+}
